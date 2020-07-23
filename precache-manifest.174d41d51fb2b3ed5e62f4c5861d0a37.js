a11dc997a5f8ffd9d400337756d90412a240d063
Saksham Singhal <singhalsaksham.mzn@gmail.com>
2020-07-23 17:19:08 +0530

    Updates

---
+++ precache-manifest.174d41d51fb2b3ed5e62f4c5861d0a37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cd0b035873027017efcd6683ccfa37",
+    "url": "/robotsdeck/index.html"
+  },
+  {
+    "revision": "30b0065bf431549c27f8",
+    "url": "/robotsdeck/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ba2fefbfc0c74efaba1e",
+    "url": "/robotsdeck/static/css/main.e456c78c.chunk.css"
+  },
+  {
+    "revision": "30b0065bf431549c27f8",
+    "url": "/robotsdeck/static/js/2.740dd7d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotsdeck/static/js/2.740dd7d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba2fefbfc0c74efaba1e",
+    "url": "/robotsdeck/static/js/main.aea8cc6c.chunk.js"
+  },
+  {
+    "revision": "c810ccda9df9065774be",
+    "url": "/robotsdeck/static/js/runtime-main.30f3fa5b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotsdeck/static/media/SEGA.12a3db8b.woff"
+  }
+]);
